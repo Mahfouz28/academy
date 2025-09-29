00b17b11cf062a9333492770db53728dd50a64be
Mahmoud Mahfouz <mahmod202350@gmail.com>
2025-09-29 18:25:30 +0300

    add take student and show student

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:academy/core/router/routs.dart';
-import 'package:academy/features/attendance/ui/pages/attendance.dart';
+import 'package:academy/features/attendance/logic/cubit/attendace_cubit.dart';
+import 'package:academy/features/attendance/ui/pages/show_attendace.dart';
+import 'package:academy/features/attendance/ui/pages/take_attendace.dart';
 import 'package:academy/features/students/logic/cubit/student_cubit.dart';
 import 'package:academy/features/students/ui/page/add_student.dart';
 import 'package:academy/features/students/ui/page/students.dart';
@@ -17,8 +19,24 @@ class AppRouter {
           ),
         );
 
+      case Routes.showAttendance:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => AttendanceCubit(),
+            child: BlocProvider(
+              create: (context) => AttendanceCubit(),
+              child: const AttendanceScreen(),
+            ),
+          ),
+        );
+
       case Routes.attendance:
-        return MaterialPageRoute(builder: (_) => const Attendance());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => AttendanceCubit(),
+            child: const Attendance(),
+          ),
+        );
 
       case Routes.students:
         return MaterialPageRoute(
--- lib/core/router/routs.dart
+++ lib/core/router/routs.dart
@@ -2,4 +2,5 @@ class Routes {
   static const String students = '/students';
   static const String addStudent = '/addStudent';
   static const String attendance = '/attendance';
+  static const String showAttendance = '/showAttendance';
 }
